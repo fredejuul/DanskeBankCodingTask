5ca827a4a908375b833f01b52d677d5e46cafe34
Frederik Juul <frederik@piboco.com>
2020-10-16 12:53:17 +0200

    Working version (without Blob)

--- DanskeBankCodingTask/Controllers/MunicipalTaxController.cs
+++ DanskeBankCodingTask/Controllers/MunicipalTaxController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using DanskeBankCodingTask.Models;
 using DanskeBankCodingTask.Models.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DanskeBankCodingTask.Controllers
 {
@@ -14,6 +16,7 @@ namespace DanskeBankCodingTask.Controllers
     public class MunicipalTaxController : ControllerBase
     {
         private readonly IDataRepository<MunicipalityTax> _dataRepository;
+        ILogger log;
 
         public MunicipalTaxController(IDataRepository<MunicipalityTax> dataRepository)
         {
@@ -25,32 +28,46 @@ namespace DanskeBankCodingTask.Controllers
         public IActionResult Get()
         {
             IEnumerable<MunicipalityTax> municipalityTaxes = _dataRepository.GetAll();
+            if (municipalityTaxes == null)
+            {
+                log.LogInformation("Unsuccessful query - no records found");
+                return NotFound("No records were found");
+            }
             return Ok(municipalityTaxes);
         }
-        // GET: api/MunicipalTax/5
-        [HttpGet("{id}", Name = "Get")]
+        // GET: api/MunicipalTax/byId?id=2
+        [HttpGet("byId", Name = "Get")]
         public IActionResult Get(int id)
         {
             MunicipalityTax municipalityTax = _dataRepository.Get(id);
             if (municipalityTax == null)
             {
+                log.LogInformation("Unsuccessful query - no records found with requested ID");
                 return NotFound("The tax record couldn't be found.");
             }
+
             return Ok(municipalityTax);
         }
-        // GET: api/MunicipalTax/byMunicipality?municipality=Copenhagen&dateTime=2016-02-02
+        // GET: api/MunicipalTax/byMunicipality?municipality=Copenhagen&dateTime=2016-12-25T00:00:00
         [HttpGet("byMunicipality", Name = "GetMunicipality")]
-        public IActionResult Get(string municipality, DateTime dateTime)
+        public IActionResult Get(string municipality, DateTime? dateTime)
         {
+            if ((municipality == null) || (!dateTime.HasValue))
+            {
+                log.LogInformation("Unsuccessful query because of missing input parameters");
+                return BadRequest("One or more input parameters format is missing");
+            }
+                
             IEnumerable<MunicipalityTax> municipalityTaxes = _dataRepository.GetAll();
             return Ok(municipalityTaxes.Where(e => e.Municipality == municipality && e.StartDate <= dateTime && e.EndDate >= dateTime));
         }
-        // POST: api/Employee
+        // POST: api/MunicipalityTax
         [HttpPost]
         public IActionResult Post([FromBody] MunicipalityTax municipalityTax)
         {
             if (municipalityTax == null)
             {
+                log.LogInformation("Unsuccessful POST because of missing input parameters");
                 return BadRequest("Tax record is not complete.");
             }
             _dataRepository.Add(municipalityTax);
@@ -59,33 +76,36 @@ namespace DanskeBankCodingTask.Controllers
                   new { Id = municipalityTax.ID },
                   municipalityTax);
         }
-        // PUT: api/Employee/5
+        // PUT: api/Municipality/5
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] MunicipalityTax municipalityTax)
         {
             if (municipalityTax == null)
             {
+                log.LogInformation("Unsuccessful PUT because of missing input parameters");
                 return BadRequest("Tax record is null.");
             }
             MunicipalityTax municipalityTaxToUpdate = _dataRepository.Get(id);
             if (municipalityTaxToUpdate == null)
             {
-                return NotFound("The Tax record couldn't be found.");
+                log.LogInformation("Unsuccessful PUT because of wrong input ID");
+                return NotFound("The tax record couldn't be found.");
             }
             _dataRepository.Update(municipalityTaxToUpdate, municipalityTax);
             return Ok(municipalityTax);
         }
-        // DELETE: api/Employee/5
+        // DELETE: api/MunicipalityTax/5
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             MunicipalityTax municipalityTax = _dataRepository.Get(id);
             if (municipalityTax == null)
             {
-                return NotFound("The Tax record couldn't be found.");
+                log.LogInformation("Unsuccessful DELETE because of wrong input ID");
+                return NotFound("The tax record couldn't be found.");
             }
             _dataRepository.Delete(municipalityTax);
-            return NoContent();
+            return Ok("The tax record has been deleted");
         }
     }
 }
